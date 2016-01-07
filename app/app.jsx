7ff51e579dbf5591f78ef817ea46f0ee98e27f44
Yuheng Wang <yhwang@amazon.com>
2016-01-07 14:43:22 -0800

    update slides

--- app/app.jsx
+++ app/app.jsx
@@ -42,6 +42,44 @@ export class App extends React.Component {
                 </Center>
             </Page>
             <Page>
+                <Center align='left'>
+                    <h2>ES6 (ES2015)</h2>
+                    <ul>
+                        <li>let and const</li>
+                        <li>arrow function and lexical this</li>
+                        <li>default, rest & spread parameter</li>
+                        <li>iterator & for..of</li>
+                        <li>template strings</li>
+                        <li>destructing</li>
+                        <li>class</li>
+                        <li>modules</li>
+                        <li>generators</li>
+                        <li>promises</li>
+                        <li>tail call</li>
+                        <li>i18n</li>
+                    </ul> 
+                </Center>
+            </Page>
+            <Page>
+                <Center align='left'>
+                    <h2>ES7 (ES2016)</h2>
+                    <ul>
+                        <li>decorators</li>
+                        <li>async/await</li>
+                    </ul>
+                </Center>
+            </Page>
+            <Page>
+                <Center align='left'>
+                    <h2>Typescript</h2>
+                    <ul>
+                        <li>superset of ES6 (and ES7 in future)</li>
+                        <li>compile to ES6/5/3</li>
+                        <li>static typed (compile time)</li>
+                    </ul>
+                </Center>
+            </Page>
+            <Page>
                 <Center>
                     <h1>Q&A</h1>
                 </Center>
